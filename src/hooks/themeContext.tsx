17891a277e16feb8c0266036dcbf6ee36082e93b
Chillie <reskialamsyah@gmail.com>
2026-03-18 15:58:52 +0800

    UPDATE: theme context to set the theme detect by system or can toggle (light/dark)

--- src/hooks/themeContext.tsx
+++ src/hooks/themeContext.tsx
@@ -1,12 +1,53 @@
-import type { Theme, ThemeContextType } from "../types/globalTypes";
-import { useContext, createContext, type ReactNode, useState } from "react";
+import type { ThemeContextType, ThemeType } from "../types/globalTypes";
+import {
+  useContext,
+  createContext,
+  type ReactNode,
+  useState,
+  useEffect,
+} from "react";
 
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 export function ThemeProvider({ children }: { children: ReactNode }) {
-  const [theme, setTheme] = useState<Theme>("system");
+  const [theme, setTheme] = useState<ThemeType>(
+    (localStorage.getItem("theme") as ThemeType) || "system",
+  );
 
-  return <ThemeContext value={{ theme, setTheme }}>{children}</ThemeContext>;
+  useEffect(() => {
+    const htmlEl = window.document.documentElement;
+
+    const applyTheme = (currentTheme: ThemeType) => {
+      let selectedTheme = currentTheme;
+
+      if (currentTheme === "system") {
+        selectedTheme = window.matchMedia("(prefers-color-scheme: dark)")
+          .matches
+          ? "dark"
+          : "light";
+      }
+
+      htmlEl.removeAttribute("data-theme");
+      htmlEl.setAttribute("data-theme", selectedTheme);
+    };
+
+    applyTheme(theme);
+    localStorage.setItem("theme", theme);
+
+    if (theme === "system") {
+      const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+      const handleChange = () => applyTheme("system");
+
+      mediaQuery.addEventListener("change", handleChange);
+      return () => mediaQuery.removeEventListener("change", handleChange);
+    }
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
 }
 
 export function UseThemeContext() {
