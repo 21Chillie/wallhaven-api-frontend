7cb0aef9809baa46afb9f58ab7e28e21c1fb89a9
Chillie <reskialamsyah@gmail.com>
2026-03-18 20:19:26 +0800

    UPDATE: add search form to main layout

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -1,3 +1,13 @@
+import { SearchForm } from "../ui/main/SearchForm";
+
 export function Main() {
-  return <></>;
+  return (
+    <>
+      <main className="flex-1 px-4 py-12">
+        <div className="mx-auto max-w-5xl">
+          <SearchForm></SearchForm>
+        </div>
+      </main>
+    </>
+  );
 }
