6cd509813291fb38be3f1c6576b73a284b49abfb
Chillie <reskialamsyah@gmail.com>
2026-03-19 21:10:13 +0800

    UPDATE: Add search input and filter form

--- src/components/ui/main/search-form/SearchForm.tsx
+++ src/components/ui/main/search-form/SearchForm.tsx
@@ -1,7 +1,33 @@
+import { IoSearch } from "react-icons/io5";
+import { FilterForm } from "./FilterForm";
+
 export function SearchForm() {
   return (
     <>
-      <h1>hello</h1>
+      <form>
+        {/* Search Query */}
+        <label
+          className="input focus-within:outline-primary/50 focus-within:border-primary/50 border-base-content/10 mb-12 w-full pr-1"
+          htmlFor="searchQuery"
+        >
+          <input
+            className="grow"
+            type="text"
+            name="searchQuery"
+            id="searchQuery"
+            placeholder="Search high-resolution wallpapers"
+          />
+
+          <button
+            className="bg-primary/90 hover:bg-primary grid place-items-center rounded-sm p-2 text-base text-gray-50 transition-colors"
+            type="button"
+          >
+            <IoSearch></IoSearch>
+          </button>
+        </label>
+
+        <FilterForm></FilterForm>
+      </form>
     </>
   );
 }
