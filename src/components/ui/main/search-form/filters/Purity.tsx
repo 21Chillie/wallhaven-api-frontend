74c4d1b6947ac6cc840b28f888227a3ee5e0eeb8
Chillie <reskialamsyah@gmail.com>
2026-03-20 00:05:37 +0800

    ADD: add Purity filter component
    Introduces a new React component for filtering search results by purity.
    This component utilizes a dropdown menu to present purity options and
    allows users to select their preferred purity level.

---
+++ src/components/ui/main/search-form/filters/Purity.tsx
@@ -0,0 +1,54 @@
+import { PURITY_OPTIONS } from "../filterParameterValues";
+import { IoIosArrowDown } from "react-icons/io";
+import type {
+  SearchParamsType,
+  BitFlagType,
+} from "../../../../../types/searchParam.types";
+
+type PurityFilterProps = {
+  params: SearchParamsType;
+  changePurityLabel: (label: string, value: BitFlagType) => void;
+};
+
+export function PurityFilter({ params, changePurityLabel }: PurityFilterProps) {
+  const { purity } = params;
+
+  return (
+    <div className="dropdown">
+      <button
+        className="btn btn-ghost text-base-content/70 hover:text-base-content focus-within:text-base-content border-base-content/15 flex items-center font-medium transition-colors duration-300"
+        tabIndex={0}
+        aria-haspopup="true"
+      >
+        <span>{purity?.label || "Purity"}</span>
+        <span>
+          <IoIosArrowDown></IoIosArrowDown>
+        </span>
+      </button>
+
+      <ul
+        tabIndex={-1}
+        className="dropdown-content menu bg-base-100 border-base-content/10 mt-2 w-48 rounded-sm border"
+      >
+        {PURITY_OPTIONS.map((opt) => {
+          const { label, value } = opt;
+
+          return (
+            <li key={label}>
+              <input
+                className="btn btn-ghost flex justify-start font-normal"
+                type="radio"
+                name="purity"
+                id="purity"
+                value={value}
+                aria-label={label}
+                checked={purity?.value === value}
+                onChange={() => changePurityLabel(label, value)}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
