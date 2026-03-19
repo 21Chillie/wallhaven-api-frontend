4471ddcb71e6e91be44b1a6469ddb1c934cd036c
Chillie <reskialamsyah@gmail.com>
2026-03-20 00:06:05 +0800

    Add: add resolution filter component
    Create a new React component for the Resolution filter in the search
    form. This component allows users to select a resolution based on the
    currently selected ratio.

---
+++ src/components/ui/main/search-form/filters/Resolution.tsx
@@ -0,0 +1,54 @@
+import type {
+  RatioKeyType,
+  ResolutionValueType,
+  SearchParamsType,
+} from "../../../../../types/searchParam.types";
+import { IoIosArrowDown } from "react-icons/io";
+import { RESOLUTION_GROUPS } from "../filterParameterValues";
+
+export function ResolutionFilter({
+  params,
+  changeResolutionLabel,
+}: {
+  params: SearchParamsType;
+  changeResolutionLabel: (ratios: ResolutionValueType) => void;
+}) {
+  const resolutionsByRatio = RESOLUTION_GROUPS[params.ratios as RatioKeyType];
+
+  return (
+    <div className="dropdown">
+      <button
+        className="btn btn-ghost text-base-content/70 hover:text-base-content focus-within:text-base-content border-base-content/10 flex items-center font-medium transition-colors duration-300"
+        tabIndex={0}
+        aria-haspopup="true"
+      >
+        <span>{params.resolutions || "Resolution"}</span>
+        <span>
+          <IoIosArrowDown></IoIosArrowDown>
+        </span>
+      </button>
+
+      <ul
+        tabIndex={-1}
+        className="dropdown-content menu bg-base-100 border-base-content/10 mt-2 w-48 rounded-sm border"
+      >
+        {resolutionsByRatio.map((value) => {
+          return (
+            <li key={value}>
+              <input
+                className="btn btn-ghost flex justify-start font-normal"
+                type="radio"
+                name="ratio"
+                id="ratio"
+                value={value}
+                aria-label={value}
+                checked={params.resolutions === value}
+                onChange={() => changeResolutionLabel(value)}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
