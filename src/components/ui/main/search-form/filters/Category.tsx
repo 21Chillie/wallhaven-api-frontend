72c397f8726737e7dad0ad8847010b2fa4a94dd3
Chillie <reskialamsyah@gmail.com>
2026-03-20 00:05:05 +0800

    ADD: Add Category filter component
    Introduces a new React component for filtering search results by
    category. This component allows users to select a category from a
    dropdown list, updating the search parameters accordingly.

---
+++ src/components/ui/main/search-form/filters/Category.tsx
@@ -0,0 +1,55 @@
+import { CATEGORY_OPTIONS } from "../filterParameterValues";
+import { IoIosArrowDown } from "react-icons/io";
+import type {
+  SearchParamsType,
+  BitFlagType,
+} from "../../../../../types/searchParam.types";
+
+type CategoryFilterProps = {
+  params: SearchParamsType;
+  changeCategoryLabel: (label: string, value: BitFlagType) => void;
+};
+
+export function CategoryFilter({
+  params,
+  changeCategoryLabel,
+}: CategoryFilterProps) {
+  return (
+    <div className="dropdown">
+      <button
+        className="btn btn-ghost border-base-content/15 text-base-content/70 hover:text-base-content focus-within:text-base-content flex items-center font-medium transition-colors duration-300"
+        tabIndex={0}
+        aria-haspopup="true"
+      >
+        <span>{params?.categories?.label || "Category"}</span>
+        <span>
+          <IoIosArrowDown />
+        </span>
+      </button>
+
+      <ul
+        tabIndex={-1}
+        className="dropdown-content menu bg-base-100 border-base-content/10 mt-2 w-48 rounded-sm border"
+      >
+        {CATEGORY_OPTIONS.map((opt) => {
+          const { label, value } = opt;
+
+          return (
+            <li key={label}>
+              <input
+                className="btn btn-ghost flex justify-start font-normal"
+                type="radio"
+                name="category"
+                id="category"
+                value={value}
+                aria-label={label}
+                checked={params?.categories?.value === value}
+                onChange={() => changeCategoryLabel(label, value)}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
