b0b8bccadabf734086775cf9113f73d48b0cb9a3
Chillie <reskialamsyah@gmail.com>
2026-03-20 00:08:47 +0800

    UPDATE: add other filter params component and apply `useFilters` hook
    Extract filter logic into `useFilters` hook and apply filter components.

--- src/components/ui/main/search-form/SearchForm.tsx
+++ src/components/ui/main/search-form/SearchForm.tsx
@@ -1,13 +1,31 @@
 import { IoSearch } from "react-icons/io5";
-import { FilterForm } from "./FilterForm";
+import { useFilters } from "../../../../hooks/useFilters.hooks";
+import {
+  CategoryFilter,
+  PurityFilter,
+  SortingFilter,
+  OrderFilter,
+  RatioFilter,
+  ResolutionFilter,
+} from "./filters";
 
 export function SearchForm() {
+  const {
+    params,
+    changeCategoryLabel,
+    changePurityLabel,
+    changeSortingLabel,
+    changeOrderLabel,
+    changeRatioLabel,
+    changeResolutionLabel,
+  } = useFilters();
+
   return (
     <>
       <form>
         {/* Search Query */}
         <label
-          className="input focus-within:outline-primary/50 focus-within:border-primary/50 border-base-content/10 mb-12 w-full pr-1"
+          className="input input-lg input-ghost outline-primary/50 focus-within:outline-primary/50 focus-within:border-primary/50 border-base-content/15 w-full pr-1.5 text-base font-medium"
           htmlFor="searchQuery"
         >
           <input
@@ -26,7 +44,46 @@ export function SearchForm() {
           </button>
         </label>
 
-        <FilterForm></FilterForm>
+        {/* Filter Parameters */}
+        <div className="border-base-content/15 mt-12 hidden border-t pt-4 md:block">
+          <ul className="border-base-content/10 flex flex-wrap gap-2">
+            <CategoryFilter
+              params={params}
+              changeCategoryLabel={changeCategoryLabel}
+            ></CategoryFilter>
+
+            <PurityFilter
+              params={params}
+              changePurityLabel={changePurityLabel}
+            ></PurityFilter>
+
+            <SortingFilter
+              params={params}
+              changeSortingLabel={changeSortingLabel}
+            ></SortingFilter>
+
+            <OrderFilter
+              params={params}
+              changeOrderLabel={changeOrderLabel}
+            ></OrderFilter>
+
+            <RatioFilter
+              params={params}
+              changeRatioLabel={changeRatioLabel}
+            ></RatioFilter>
+
+            <ResolutionFilter
+              params={params}
+              changeResolutionLabel={changeResolutionLabel}
+            ></ResolutionFilter>
+
+            <div className="divider divider-horizontal mx-1 h-8 self-center"></div>
+
+            <button className="btn btn-primary btn-soft text-sm">
+              Apply filters
+            </button>
+          </ul>
+        </div>
       </form>
     </>
   );
