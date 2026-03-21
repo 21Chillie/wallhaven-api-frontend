2fada13f661b7f533f05445194d68b94ec7642ce
Chillie <reskialamsyah@gmail.com>
2026-03-21 16:47:47 +0800

    UPDATE: Add react-query and react-hot-toast providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Header } from "./components/layout/Header";
 import { Main } from "./components/layout/Main";
 import { ThemeProvider } from "./hooks/themeContext.hooks";
+import { Toaster } from "react-hot-toast";
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
     <>
+      <Toaster></Toaster>
+
       <ThemeProvider>
         <Header></Header>
       </ThemeProvider>
-      <Main></Main>
+
+      <QueryClientProvider client={queryClient}>
+        <Main></Main>
+      </QueryClientProvider>
     </>
   );
 }
