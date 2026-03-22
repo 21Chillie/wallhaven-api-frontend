030d91c1058a13987d815b53012dc2537e184b7a
Chillie <reskialamsyah@gmail.com>
2026-03-22 17:10:35 +0800

    TEST: Display wallpaper search results (only result ID for testing)
    Render the IDs of fetched wallpapers to the UI.

--- src/components/ui/main/image-card/WallpaperResults.tsx
+++ src/components/ui/main/image-card/WallpaperResults.tsx
@@ -1,3 +1,14 @@
+import { UseSearchContext } from "../../../../hooks/searchContext.hooks";
+
 export function WallpaperResults() {
-  return <></>;
+  const { query } = UseSearchContext();
+  const { data } = query;
+
+  return (
+    <>
+      {data?.data.map((item) => {
+        return <p>{item.id}</p>;
+      })}
+    </>
+  );
 }
