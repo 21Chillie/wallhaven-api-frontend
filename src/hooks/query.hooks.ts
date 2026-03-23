4213595eb8322cebf8bcb6c4179648a89f4a0712
Chillie <reskialamsyah@gmail.com>
2026-03-23 20:07:04 +0800

    UPDATE: Refactor query hook to use infinite query
    Replaces `useQuery` with `useInfiniteQuery` to enable fetching
    subsequent pages of search results. Includes logic for
    `getNextPageParam` and exposes relevant properties for infinite
    scrolling.

--- src/hooks/query.hooks.ts
+++ src/hooks/query.hooks.ts
@@ -1,24 +1,56 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import type { SearchParamsType } from "../types/searchParam.types";
 import apiService from "../services/api.service";
 import toast from "react-hot-toast";
+import type { WallhavenMeta } from "../types/apiResponse.types";
 
 function useTanstackQuery(params: Partial<SearchParamsType>) {
-  const { isPending, data, isError, error, isFetching } = useQuery({
+  // const { isPending, data, isError, error, isFetching } = useQuery({
+  //   queryKey: ["wallpapers", params],
+  //   queryFn: async () => await apiService.search(params),
+  //   enabled: params.q ? params.q.length > 0 : false,
+  //   staleTime: 1000 * 60 * 5,
+  //   // retry: false,
+  // });
+
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    isPending,
+    isError,
+    fetchStatus,
+    error,
+    isFetching,
+  } = useInfiniteQuery({
     queryKey: ["wallpapers", params],
-    queryFn: async () => await apiService.search(params),
+    queryFn: ({ pageParam }) => apiService.search(params, pageParam),
+    initialPageParam: 1,
     enabled: params.q ? params.q.length > 0 : false,
+    // This function tells the hook how to get the next page number
+    getNextPageParam: (lastPage) => {
+      const { current_page, last_page } = lastPage.meta as WallhavenMeta;
+      return current_page < last_page ? current_page + 1 : undefined;
+    },
     staleTime: 1000 * 60 * 5,
-    // retry: false,
   });
 
   if (isError) {
     toast.error(error.message);
   }
 
-  // DEBUG
-  // console.log(data ? "query success" : "query failed");
-  return { isPending, data, isError, error, isFetching };
+  return {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    isPending,
+    isError,
+    fetchStatus,
+    error,
+    isFetching,
+  };
 }
 
 export default useTanstackQuery;
