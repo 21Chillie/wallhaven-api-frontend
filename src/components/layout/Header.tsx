19520920806123bac79f8562714732130dc55b6a
Chillie <reskialamsyah@gmail.com>
2026-03-24 02:44:29 +0800

    UPDATE: change the navbar header text to italic

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -7,7 +7,7 @@ export function Header() {
       <header className="border-base-300 border-b p-4 shadow-sm">
         <div className="mx-auto flex max-w-7xl items-center justify-between gap-4">
           <div>
-            <h3 className="text-2xl font-bold">Wallpaper</h3>
+            <h3 className="text-2xl font-bold italic">Wallpaper</h3>
           </div>
 
           <div className="flex gap-3">
