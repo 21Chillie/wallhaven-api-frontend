5b378f8c0d45426ee9f223afd2b781e46145838e
Chillie <reskialamsyah@gmail.com>
2026-03-24 16:24:05 +0800

    UPDATE: add proxy rules to rewrite the url path for images (thumbnail and full)

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,23 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
+      "/api/wallhaven/full": {
+        target: "https://w.wallhaven.cc/full",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/wallhaven\/full/, ""),
+        headers: {
+          Referer: "https://wallhaven.cc/",
+        },
+      },
       "/api/wallhaven": {
         target: "https://wallhaven.cc/api/v1",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/wallhaven/, ""),
+      },
+      "/api/th": {
+        target: "https://th.wallhaven.cc",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/th/, ""),
       },
     },
     allowedHosts: true,
