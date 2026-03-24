156cfca883aeee3baf1dd2b26d911999bb536283
Chillie <reskialamsyah@gmail.com>
2026-03-24 16:25:24 +0800

    UPDATE: dotenv import/export

---
+++ src/utils/importEnv.ts
@@ -0,0 +1,6 @@
+const BASE_URL: string = import.meta.env.VITE_API_BASE_URL;
+const ENV: string = import.meta.env.VITE_BUN_ENV;
+const API_KEY: string = import.meta.env.VITE_API_KEY;
+const PROD_BASE_URL: string = import.meta.env.VITE_PROD_BASE_URL;
+
+export {BASE_URL, ENV, API_KEY, PROD_BASE_URL}
