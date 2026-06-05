227bffcded172e7ea6c5ef53e9c01d50c417b22d
Chillie <reskialamsyah@gmail.com>
2026-06-06 03:02:28 +0800

    update: add search field component in SearchFilter ui components and also add function to handle submit

--- src/components/ui/SearchFilter.tsx
+++ src/components/ui/SearchFilter.tsx
@@ -1,3 +1,27 @@
+import { useSearchForm } from "~/hooks/useSearchForm";
+
 export default function SearchFilter() {
-  return <div></div>;
+  const form = useSearchForm();
+  const handleSubmit = (e: React.SubmitEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    form.handleSubmit();
+  };
+
+  return (
+    <section
+      id="search-form"
+      className="bg-base-100 border-base-300 my-8 border p-6 shadow-sm max-xl:mx-4">
+      <form onSubmit={handleSubmit}>
+        <form.AppField
+          name="q"
+          validators={{
+            onBlur: ({ value }) =>
+              !value ? "Search field cannot be empty!" : undefined,
+          }}>
+          {(field) => <field.SearchField />}
+        </form.AppField>
+      </form>
+    </section>
+  );
 }
