3363b8baf5c775491b70a5c685551735a9d75e8d
Chillie <reskialamsyah@gmail.com>
2026-06-06 03:02:47 +0800

    update: add params object to use for fetch later

--- src/components/ui/WallpaperResult.tsx
+++ src/components/ui/WallpaperResult.tsx
@@ -1,3 +1,7 @@
 export default function WallpaperResult() {
+  const params = new URLSearchParams(window.location.search);
+  const values = Object.fromEntries(params.entries());
+  console.log(values);
+
   return <div></div>;
 }
