e7b2e149e707372e8ff8d17cc407280dbe08f19f
Chillie <reskialamsyah@gmail.com>
2026-06-06 02:58:33 +0800

    update: add toast component in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,37 @@
 import { ThemeProvider } from "@store/themeProvider.tsx";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Toaster } from "react-hot-toast";
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Toaster
+      toastOptions={{
+        success: {
+          style: {
+            borderRadius: 0,
+          },
+          className: "!text-base-content !font-medium !bg-base-100 !shadow-md",
+          iconTheme: {
+            primary: "var(--color-primary)",
+            secondary: "var(--color-primary-content)",
+          },
+        },
+        error: {
+          style: {
+            borderRadius: 0,
+          },
+          className: "!text-base-content !font-medium !bg-base-100 !shadow-md",
+          iconTheme: {
+            primary: "var(--color-error)",
+            secondary: "var(--color-error-content)",
+          },
+        },
+      }}
+    />
+
     <ThemeProvider>
       <App />
     </ThemeProvider>
