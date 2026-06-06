338ca971dcfae0ff602601b608406b5c752f386a
Chillie <reskialamsyah@gmail.com>
2026-06-07 06:55:29 +0800

    update: add queryClient provider and fix layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
+import ButtonScrollTop from "@components/BtnScrollTop";
 import Navbar from "@components/ui/Navbar";
 import SearchFilter from "@components/ui/SearchFilter";
 import WallpaperResult from "@components/ui/WallpaperResult";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
     <div className="bg-base-200 min-h-screen w-screen flex-col">
       <Navbar />
 
-      <main className="flex-1">
+      <main className="flex-1 pb-4">
         <SearchFilter />
-        <WallpaperResult />
+
+        <QueryClientProvider client={queryClient}>
+          <WallpaperResult />
+        </QueryClientProvider>
       </main>
+
+      <ButtonScrollTop />
     </div>
   );
 }
