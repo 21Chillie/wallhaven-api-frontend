c7d56fa8d28cd899d641ed361c419d7080254240
Chillie <reskialamsyah@gmail.com>
2026-06-07 06:59:08 +0800

    update: fix layout and text size on navbar

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -4,15 +4,15 @@ import { SiGithub } from "@icons-pack/react-simple-icons";
 export default function Navbar() {
   return (
     <header className="bg-base-300">
-      <nav className="mx-auto flex max-w-6xl items-center justify-between py-4 max-xl:px-4">
+      <nav className="mx-auto flex max-w-6xl items-center justify-between py-4 max-xl:px-4 gap-4">
         {/* Navbar Brand */}
         <div className="space-y-1">
           <a href="/">
-            <h3 className="text-3xl font-bold tracking-tight uppercase">
+            <h3 className="text-2xl font-bold tracking-tight uppercase md:text-3xl">
               Wallpapers
             </h3>
           </a>
-          <p className="text-base-content/70 text-sm leading-relaxed md:text-base">
+          <p className="text-base-content/70 w-3/4 sm:w-full text-xs leading-relaxed md:text-base">
             Find high-resolution wallpapers
           </p>
         </div>
