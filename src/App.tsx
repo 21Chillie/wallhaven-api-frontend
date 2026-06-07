d4aa7612914d7ca77caaffff876b3b7308d9065a
Chillie <reskialamsyah@gmail.com>
2026-06-07 20:22:47 +0800

    update: add APIKeyModal component into app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import ButtonScrollTop from "@components/BtnScrollTop";
+import { APIKeyModal } from "@components/ModalApiKey";
 import ModalWallpaperCard from "@components/ModalWallpaperCard";
 import Navbar from "@components/ui/Navbar";
 import SearchFilter from "@components/ui/SearchFilter";
@@ -12,17 +13,18 @@ function App() {
     <div className="bg-base-200 min-h-screen w-screen flex-col">
       <Navbar />
 
-      <main className="flex-1 pb-4">
+      <main className="relative flex-1 pb-4">
         <SearchFilter />
 
         <QueryClientProvider client={queryClient}>
           <WallpaperResult />
         </QueryClientProvider>
+
+        <ButtonScrollTop />
       </main>
 
-      <ButtonScrollTop />
-
       <ModalWallpaperCard />
+      <APIKeyModal />
     </div>
   );
 }
